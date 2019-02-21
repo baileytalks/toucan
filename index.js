c842bfce00c678df064b1dde9e40df8dfe89c1be
Bailey Kursar <bkursar@gmail.com>
2019-02-21 11:32:37 +0000

    Added cache control with express-cache-controller

--- index.js
+++ index.js
@@ -2,9 +2,14 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const path = require('path');
+const cacheControl = require('express-cache-controller');
 
 app.set('view engine', 'ejs');
 
 app.use(express.static('public'));
 
 app.listen(port, () => console.log(`Toucan listening on port ${port}!`))
+
+app.use(cacheControl({
+  maxAge: 60
+}));
